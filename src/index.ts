8e34182df39134d4098c53a06f059413ed82465a
Sébastien Doucet <sebdoucet@gmail.com>
2022-01-22 13:08:59 -0500

    Add tryInvoke

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./deferred";
 export * from "./delayed";
 export * from "./debouncer";
+export * from "./timeout-source";
 export * from "./abort-token";
 export * as AsyncHelper from "./helpers";
 export * from "./task";
--- src/timeout-source.ts
+++ src/timeout-source.ts
@@ -5,15 +5,29 @@ export class TimeoutSource implements IDisposable {
 
     get hasPendingCall(): boolean { return this._timer !== -1; }
 
+    /** Invoke the pending call and cancel any pending ones */
     invoke(callback: Action, timeout: number): void {
         this.clear();
+        this.invokeCore(callback, timeout);
+    }
 
+    /** Invoke the provided callback if there is no pending call */
+    tryInvoke(callback: Action, timeout: number): boolean {
+        if (this._timer === -1) {
+            this.invokeCore(callback, timeout);
+            return true;
+        }
+        return false;
+    }
+
+    private invokeCore(callback: Action, timeout: number): void {
         this._timer = self.setTimeout(() => {
             this._timer = -1;
             callback();
         }, timeout);
     }
 
+    /** Cancel any pending calls */
     clear(): void {
         if (this._timer !== -1) {
             clearTimeout(this._timer);
